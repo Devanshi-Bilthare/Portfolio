7db1f797f83776c5be0f7b83843628f53f100df7
Devanshi Bilthare <devanshibilthare@gmail.com>
2024-01-08 13:10:05 +0530

    added updated resume

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = () => {
   return (
     <nav>
     <div className="left_nav">
-        <h3>DB</h3>
+       <Link to='/'><h3>DB</h3></Link>
     </div>
     <div className="right_nav">
         <Link to='/'><p>Home</p></Link>
         <Link to='/about'><p>About</p></Link>
         <Link to='/project'><p>Projects</p></Link>
-        <a href="https://drive.google.com/file/d/1HD9lFme4_NFskpeAsFb3b0HlgJ2UK6z7/view?usp=sharing"><Button>Resume</Button></a>
+        <a href="https://drive.google.com/file/d/1a_o3lK87exe28rCHBWoBMZH1XK57iH1Z/view?usp=sharing"><Button>Resume</Button></a>
     </div>
 </nav>
   )
