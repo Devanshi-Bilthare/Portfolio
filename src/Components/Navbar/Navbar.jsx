ed79fbb16c7c8f18e560b929df50a3703e83f31f
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2025-05-30 16:52:16 +0530

    updated navbar responsiveness

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,38 +1,51 @@
 import React, { useState } from 'react';
-import "./Navbar.css"
-import { Link } from 'react-router-dom'
+import "./Navbar.css";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-
   const [showHiddenNav, setShowHiddenNav] = useState(false);
 
   const toggleHiddenNav = () => {
-    console.log('clicked')
     setShowHiddenNav(!showHiddenNav);
-  }
+  };
 
+  const handleNavItemClick = () => {
+    setShowHiddenNav(false);
+  };
 
   return (
     <nav>
-    <div className="left_nav">
-       <Link to='/'><h3>DB</h3></Link>
-    </div>
-    <div className="right_nav">
+      <div className="left_nav">
+        <Link to='/'><h3>DB</h3></Link>
+      </div>
+
+      <div className="right_nav">
         <Link to='/'><p>Home</p></Link>
         <Link to='/about'><p>About</p></Link>
         <Link to='/project'><p>Projects</p></Link>
-        <Link to="https://drive.google.com/file/d/1bn1V7-ylfhq9YehY0-eJlZeXBhCpPEMx/view?usp=sharing"><button>Resume</button></Link>
-    </div>
-    <div className="btn" onClick={toggleHiddenNav}>{showHiddenNav ?<i class="ri-close-large-fill"></i> : <i class="ri-menu-3-line"></i>}</div>
-        <div className={`hidden_nav ${showHiddenNav ? "show" : "hide"}`}>
-            <Link to='/'><p>Home</p></Link>
-            <Link to='/about'><p>About</p></Link>
-            <Link to='/project'><p>Projects</p></Link>
-            <Link to="https://drive.google.com/file/d/1N2-BdpL0Cizu-Om3PZ5eRf43cYEQRM9G/view?usp=sharing"><button>Resume</button></Link>
-        </div>
-      
-</nav>
-  )
-}
+        <Link to="https://drive.google.com/file/d/1bn1V7-ylfhq9YehY0-eJlZeXBhCpPEMx/view?usp=sharing">
+          <button>Resume</button>
+        </Link>
+      </div>
 
-export default Navbar
+      <div className="btn" onClick={toggleHiddenNav}>
+        {showHiddenNav ? (
+          <i className="ri-close-large-fill"></i>
+        ) : (
+          <i className="ri-menu-3-line"></i>
+        )}
+      </div>
+
+      <div className={`hidden_nav ${showHiddenNav ? "show" : "hide"}`}>
+        <Link to='/' onClick={handleNavItemClick}><p>Home</p></Link>
+        <Link to='/about' onClick={handleNavItemClick}><p>About</p></Link>
+        <Link to='/project' onClick={handleNavItemClick}><p>Projects</p></Link>
+        <Link to="https://drive.google.com/file/d/1N2-BdpL0Cizu-Om3PZ5eRf43cYEQRM9G/view?usp=sharing" onClick={handleNavItemClick}>
+          <button>Resume</button>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
